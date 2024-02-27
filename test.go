9fa44bfddd7063519e728b55e739d191d792f967
ekaterynah <katyagumeniuk12@gmail.com>
2024-02-27 21:04:44 +0200

    Add print

--- test.go
+++ test.go
@@ -1,27 +1,30 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 type TimeResponse struct {
-  Time string `json:"time"`
+	Time string `json:"time"`
 }
 
 func main() {
-  http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-    currentTime := time.Now().Format(time.RFC3339)
-    response := TimeResponse{Time: currentTime}
-    jsonResponse, err := json.Marshal(response)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(jsonResponse)
-  })
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now().Format(time.RFC3339)
+		response := TimeResponse{Time: currentTime}
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonResponse)
+	})
 
-  http.ListenAndServe(":8795", nil)
+	fmt.Println("Сервер запущено на http://localhost:8795")
+
+	http.ListenAndServe(":8795", nil)
 }
